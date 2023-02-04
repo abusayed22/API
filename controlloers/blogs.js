2f7b04acabd1c57989a93413c2a6544481074248
Abu Sayed <abusayed.in22@gmail.com>
2023-02-04 20:44:06 +0600

    advance filtering done

--- controlloers/blogs.js
+++ controlloers/blogs.js
@@ -2,25 +2,34 @@ const blogModel = require('../models/blog');
 // const blogjson = require('../blogs.json')
 
 
-const getAllBlogs = async(req,res) => {
-    const {name} = req.query
-    const queryObject = {}
+const getAllBlogs = async (req, res) => {
 
-    if(name) {
-        queryObject.name = name;
+    // TODO:
+    const { name, heading, contend } = req.query
+    const queryObject = {}
+    if (name) {
+        queryObject.name = {$regex: name, $options: 'i'};
     }
 
+    if (heading) {
+        queryObject.heading = heading
+    }
+    if (contend) {
+        queryObject.contend = contend
+    }
+    // TODO:
+
     const myData = await blogModel.find(queryObject);
-    
-    res.status(200).json({myData})
+
+    res.status(200).json({ myData })
 }
 
-const getAllBlogsTesting = async(req,res) => {
+const getAllBlogsTesting = async (req, res) => {
     // const myData = await blogModel.find(req.query)
-    res.status(200).json({msg: "I am Testing blogs"});
+    res.status(200).json({ msg: "I am Testing blogs" });
 }
 
 
 
 
-module.exports = { getAllBlogs, getAllBlogsTesting};
+module.exports = { getAllBlogs, getAllBlogsTesting };
